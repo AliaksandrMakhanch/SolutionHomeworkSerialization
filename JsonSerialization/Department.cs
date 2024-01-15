b5ed4f2da729224a50dceed5ea0761dd6eebe584
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2024-01-15 15:08:04 +0300

    Accessibility change

--- JsonSerialization/Department.cs
+++ JsonSerialization/Department.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace JsonSerialization
 {
-    internal class Department
+    public class Department
     {
        public string DepartmentName {  get; set; }
     }
