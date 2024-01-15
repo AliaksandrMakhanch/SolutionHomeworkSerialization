1b9a655b848df6b61fd47f0a10aed623f457cb68
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2024-01-15 15:06:59 +0300

    Change accessibility

--- BinarySerialization/Employee.cs
+++ BinarySerialization/Employee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BinarySerialization
 {
-    internal class Employee
+    public class Employee
     {
        public string EmployeeName { get; set; }
     }
